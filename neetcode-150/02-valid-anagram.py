b54e0f15c344d1dffcb3a00afc6a0673523a2fbf
imammunif <imammunif17@gmail.com>
2025-12-05 18:20:21 +0700

    valid anagram

---
+++ neetcode-150/02-valid-anagram.py
@@ -0,0 +1,92 @@
+s = "racecar"
+t = "carrace"
+
+def isAnagram(s: str, t: str) -> bool:
+    listS = list(s) # ["r","a","c","e","c","a","r"]
+    listT = list(t)
+    # hashS = [{'char': 'r', 'count': 1}, {'char': 'a', 'count': 1}]
+    hashS = [] # {r},{a},{c},{e}
+    hashT = [] # {c},{a},{r},{e},
+    equal = False
+
+    for char in listS:
+        match = False
+        # print(f'Char {char}')
+        if hashS != []:
+            # print(f'   {hashS}')
+            for hash in hashS:
+                # for key, val in hash.items():
+                # print(f'   checking {char} with {hash["char"]}')
+                if hash["char"] == char:
+                    match = True
+                    hash["count"] += 1
+                    # print(f'  > hash char {hash["char"]} is equal with {char}, count + 1')
+                    # print(f'   {match}')
+                    break
+            if match == False:
+                # print('  > buat baru')
+                new = {'char': char, 'count': 1}
+                hashS.append(new)
+        else:
+            # print('   Hash kosong, mari kita isi')
+            new = {'char': char, 'count': 1}
+            hashS.append(new)
+            # print(f'   {hashS}')
+
+    for char in listT:
+        match = False
+        # print(f'Char {char}')
+        if hashT != []:
+            # print(f'   {hashT}')
+            for hash in hashT:
+                # for key, val in hash.items():
+                # print(f'   checking {char} with {hash["char"]}')
+                if hash["char"] == char:
+                    match = True
+                    hash["count"] += 1
+                    # print(f'  > hash char {hash["char"]} is equal with {char}, count + 1')
+                    # print(f'   {match}')
+                    break
+            if match == False:
+                # print('  > buat baru')
+                new = {'char': char, 'count': 1}
+                hashT.append(new)
+        else:
+            # print('   Hash kosong, mari kita isi')
+            new = {'char': char, 'count': 1}
+            hashT.append(new)
+            # print(f'   {hashT}')
+
+    if hashS != []:
+        if hashT != []:
+            # {r},{a},{c},{e}
+            eq = False
+            for dictS in hashS:
+                # {c},{a},{r},{e},
+                for dictT in hashT:
+                    print(f'checking {dictS["char"]} with {dictT["char"]}')
+                    if dictS["char"] == dictT["char"]:
+                        # founded the same char
+                        if dictS["count"] == dictT["count"]:
+                            eq = True
+                            # has the same value
+                            print(f'equal {eq}')
+                            break # stop the 2nd list loop
+                        else:
+                            eq = False
+                            print(f'equal {eq}')
+                            # print("diff val")
+                            # different value
+                            # equal = False
+                            break
+                if eq == False:
+                    print("Found false stop!!")
+                    break
+            equal = eq
+
+    return hashS, hashT, equal
+
+print(isAnagram(s,t))
+
+# test = [{"char": "r","count": 1},{"char": "r","count": 1}]
+# print(type(test[0]))
