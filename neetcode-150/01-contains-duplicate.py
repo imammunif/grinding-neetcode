16fb3a2966f65a6fe2c66f938b27fd30e72706c0
imammunif <imammunif17@gmail.com>
2025-12-05 20:19:41 +0700

    using set instead of list

--- neetcode-150/01-contains-duplicate.py
+++ neetcode-150/01-contains-duplicate.py
@@ -1,14 +1,14 @@
 nums = [1, 2, 3, 3]
 
 def hasDuplicate(nums: list) -> bool:
-    hashMap = []
+    hashMap = set()
     for num in nums:
         print(f'checking {num}')
         if num in hashMap:
             # print("True")
             return True
         else:
-            hashMap.append(num)
+            hashMap.add(num)
     # print("False")
     return False  
 
